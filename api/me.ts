f8af9f55f4c16585559709abe2c252b89590a834
Yuichi Yazaki <visualizingjp@gmail.com>
2025-12-07 12:28:22 +0900

    first commit

---
+++ api/me.ts
@@ -0,0 +1,7 @@
+// api/me.ts
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { createClient } from "@supabase/supabase-js";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({ ok: true, message: "API 動いています" });
+}
